1c3ace2fdd2fb53a372c1a675c4e267702882262
Lio <liobensignor@gmail.com>
2024-08-11 16:57:11 -0300

    Test v2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-var rellax = new Rellax('.parallax');
+var rellax = new Rellax('.parallax', {
+  speed: -2,
+  center: true
+});
+
+document.getElementById('openNav').addEventListener('click', function() {
+  document.querySelector('.nav').classList.toggle('open');
+});
