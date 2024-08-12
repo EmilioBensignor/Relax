d2face47d4bf4b5293fade080e4d8e143ca2545c
Lio <liobensignor@gmail.com>
2024-08-11 22:35:22 -0300

    Test v3 Todo fallido

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-var rellax = new Rellax('.parallax', {
-  speed: -2,
-  center: true
-});
+const rellax = new Rellax('.rellax');
 
 document.getElementById('openNav').addEventListener('click', function() {
   document.querySelector('.nav').classList.toggle('open');
