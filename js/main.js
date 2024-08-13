661b4e00774fb0eaf21321430608257d942a261c
Emilio Bensignor <liobensignor@gmail.com>
2024-08-12 23:52:26 -0300

    Test v4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-const rellax = new Rellax('.rellax');
-
 document.getElementById('openNav').addEventListener('click', function() {
   document.querySelector('.nav').classList.toggle('open');
+});
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.header');
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+  var maxScroll = 500;
+  var opacity = 1 - (scrollTop / maxScroll);
+
+  if (opacity < 0) opacity = 0;
+  if (opacity > 1) opacity = 1;
+
+  header.style.opacity = opacity;
+
+  // Verificación
+  console.log('ScrollTop:', scrollTop, 'Opacity:', opacity);
 });
